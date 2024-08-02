9a26ec9b82caaa7c6438f15e39f6f3a9a3e7671d
yaowalukth <16917yaowaluk@gmail.com>
2024-08-02 11:51:29 +0700

    Create LAB5.1.cpp

---
+++ LAB5.1.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    float score = 0 ;
+    printf( "Input your score : " ) ;
+    if(scanf( "%f" , &score  ) !=1 ) {
+           printf( "Please enter number only") ;
+    }   
+            else if( score >= 80 ) {
+                printf( "Grade : A" ) ;
+            }else if( score >= 75 && score < 80 ) {
+                printf( "Grade : B+" ) ;
+            }else if( score >= 70 && score < 75 ) {
+                printf( "Grade : B" ) ;
+            }else if( score >= 65 && score < 70 ) {
+                printf( "Grade : C+" ) ;
+            }else if( score >= 60 && score < 65 ) {
+                printf( "Grade : C" ) ;
+            }else if( score >= 55 && score < 60 ) {
+                printf( "Grade : D+" ) ;
+            }else if( score >= 50 && score < 55 ) {
+                printf( "Grade : D" ) ;
+            }else{
+                printf( "Grade : F \n" ) ;
+            }
+    return 0 ;
+}
