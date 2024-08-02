733a40d31a22821cfdac73c7c5bce9e1ef463928
yaowalukth <16917yaowaluk@gmail.com>
2024-08-02 11:02:44 +0700

    Update LAB5-1.cpp

--- LAB5-1.cpp
+++ LAB5-1.cpp
@@ -3,24 +3,24 @@
 int main() {
     float score = 0 ;
     printf( "Input your score : " ) ;
-    scanf( "%f" , &score ) ;
-    printf( "Grade : " ) ;
-    if( score >= 80 ){
-        printf( "A" ) ;
-    }else if( score >= 75 && score < 80 ) {
-        printf( "B+" ) ;
-    }else if( score >= 70 && score < 75 ) {
-        printf( "B" ) ;
-    }else if( score >= 65 && score < 70 ) {
-        printf( "C+" ) ;
-    }else if( score >= 60 && score < 65 ) {
-        printf( "C" ) ;
-    }else if( score >= 55 && score < 60 ) {
-        printf( "D+" ) ;
-    }else if( score >= 50 && score < 55 ) {
-        printf( "D" ) ;
-    }else{
-        printf( "F" ) ;
-    }
+    if(scanf( "%f" , &score  ) !=1 ) {
+           printf( "Please enter number only") ;
+    }else if( score >= 80 ) {
+                printf( "Grade : A" ) ;
+            }else if( score >= 75 && score < 80 ) {
+                printf( "Grade : B+" ) ;
+            }else if( score >= 70 && score < 75 ) {
+                printf( "Grade : B" ) ;
+            }else if( score >= 65 && score < 70 ) {
+                printf( "Grade : C+" ) ;
+            }else if( score >= 60 && score < 65 ) {
+                printf( "Grade : C" ) ;
+            }else if( score >= 55 && score < 60 ) {
+                printf( "Grade : D+" ) ;
+            }else if( score >= 50 && score < 55 ) {
+                printf( "Grade : D" ) ;
+            }else{
+                printf( "Grade : F \n" ) ;
+            }
     return 0 ;
 }
