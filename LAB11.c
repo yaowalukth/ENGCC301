1b6337e00e29f0cd8e7820b5488f30ddbb14c4bf
yaowalukth <16917yaowaluk@gmail.com>
2024-10-05 20:08:30 +0700

    Update and rename LAB11.cpp to LAB11.c

--- LAB11.c
+++ LAB11.c
@@ -5,9 +5,9 @@ int power( int base , int e ) {
     int result = 1 ;
     for ( int i = 0 ; i < e ; i++ ) {
         result *= base ; 
-    }//end for
+    }
     return result;
-}//end function
+}
 
 void Armstrong( char Number[] ) {
     int c = strlen( Number ) ;
@@ -17,32 +17,27 @@ void Armstrong( char Number[] ) {
     for( i = 0 ; i < c ; i ++ ) {
         int num = Number[ i ] - '0' ;
         ans += power( num , c ) ;
-    }//end for
-    printf( "%d \n", ans ) ;
+    }
 
     int Usernumber = 0 ;
     for ( i = 0 ; i < c ; i++ ) {
         Usernumber = Usernumber * 10 + ( Number[ i ] - '0' ) ;
-    }//end for
+    }
 
     if( Usernumber != ans ) {
         printf( "Not Pass" ) ;
     } else {
         printf( "Pass" ) ;
-    }//end if-else
-}//end function
+    }
+}
 
 int main() {
 
     char Number[ 100 ] ;
-    printf( "-------------------------------------\n" ) ;
-    printf( "Enter your number : " ) ;
+    printf( "Enter number : " ) ;
     scanf( "%s" , Number ) ;
-    printf( "Your number is : %s", Number ) ;
-    printf( "\n-------------------------------------\n" ) ;
-
     Armstrong( Number ) ;
-    printf( "\n-------------------------------------\n" ) ;
+    
     
     return 0 ;
-}//end function
+}
