47a86a38f61b0452da3a965170fe08ef1a269e2f
yaowalukth <16917yaowaluk@gmail.com>
2024-10-05 20:03:12 +0700

    Update and rename LAB10.cpp to LAB10.c

--- LAB10.c
+++ LAB10.c
@@ -5,15 +5,12 @@
 int main() {
 
     char Word[ 100 ] ;
-    printf( "Enter your word : " ) ;
+    printf( "Enter word : " ) ;
     scanf( "%s", Word ) ;
-    printf( "Your word is : %s \n", Word ) ;
 
     int e = strlen( Word ) - 1 ;
-    printf( "Size of Word is : %d \n", e + 1 ) ;
-
-    int s = 0 ; //ค่าเริ่มต้นของตำอักษรแรก
-    int n = 1 ; //ค่าบูลีน
+    int s = 0 ;
+    int n = 1 ;
     while ( s < e ) {
         if( tolower( Word[ s ] ) != tolower( Word[ e ] ) ) {
             n = 0 ;
@@ -21,14 +18,14 @@ int main() {
         } else {
             s ++ ;
             e -- ;
-        }//end if-else
-    }//end while
+        }
+    }
 
     if( n == 0 ) {
         printf( "Not Pass \n" ) ;
     } else {
         printf( "Pass \n" ) ;
-    }//end if-else
+    }
 
     return 0 ;
-}//end main function
+}
