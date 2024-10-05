f5429cf4038b90d4d5638794eeb1662c78f354b0
yaowalukth <16917yaowaluk@gmail.com>
2024-10-05 18:36:00 +0700

    Update LAB4.c

--- LAB4.c
+++ LAB4.c
@@ -3,17 +3,16 @@
 int main() {
     char ID[ 10 ] ;
     int WorkingHour, Salary ;
-    printf( "Input the Employees ID(Max. 10 chars): " ) ;
+    printf( "\n\nInput the Employees ID(Max. 10 chars): " ) ;
     scanf( "%s", ID ) ;
     printf( "Input the working hrs: " ) ;
     scanf( "%d", &WorkingHour ) ;
     printf( "Salary amount/hr: " ) ;
     scanf( "%d", &Salary ) ;
 
-    printf( "\n---------------\n" ) ;
     printf( "Employees ID = %s\n", ID ) ;
-    printf( "Salary = %d THB", WorkingHour * Salary * 22 ) ;
+    printf( "Salary = U$ %d", WorkingHour * Salary ) ;
 
-    printf( "\n" ) ;
+    printf( "\n\n" ) ;
     return 0 ;
 }
