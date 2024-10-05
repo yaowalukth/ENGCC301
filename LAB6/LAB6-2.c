000297b178f2b4472e3e00048466bbf63aa68b77
yaowalukth <16917yaowaluk@gmail.com>
2024-10-05 22:46:47 +0700

    Update LAB6-2.c

--- LAB6/LAB6-2.c
+++ LAB6/LAB6-2.c
@@ -7,11 +7,11 @@ int main() {
     printf( "Series:" ) ;
     if(number % 2 == 1) {
         for(int i = 1 ; i <= number ; i += 2 ) {
-            printf( "%d ", i ) ;
+            printf( " %d ", i ) ;
         }
     }else if(number % 2 != 1) {
         for(int i = number ; i >= 0 ; i -= 2) {
-            printf( "%d " , i ) ;
+            printf( " %d " , i ) ;
         }
     }
     return 0 ;
