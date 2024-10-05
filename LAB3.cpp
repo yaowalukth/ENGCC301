a736f955e000db10cca05f94ede9f50437004979
yaowalukth <16917yaowaluk@gmail.com>
2024-10-05 17:33:25 +0700

    Update LAB3.cpp

--- LAB3.cpp
+++ LAB3.cpp
@@ -16,27 +16,27 @@ int main() {
         scanf( "%s %d %f", Name[i],  &Quantity[i], &UnitPrice[i] ) ;
 
         while( strcmp(Name[i], "END" ) != 0) {
-            i++;
+            i ++;
             printf( "Enter name, Quantity, and price for item %d [END 0 0] : ", i + 1 ) ;
             scanf( "%s %d %f", Name[i], &Quantity[i], &UnitPrice[i] ) ;
-        }//end while
+        }
 
         NumberOfItems = i ;
         TotalAmount = 0.0 ;
-        for( i = 0 ; i < NumberOfItems ; i++ ) {
+        for( i = 0 ; i < NumberOfItems ; i ++ ) {
             TotalPrice[i] = Quantity[i] * UnitPrice[i] ;
             TotalAmount += TotalPrice[i] ;
-        }//end for
+        }
         printf( "-------------------------------------------------------------- \n" ) ;
         printf( "%-20s %11s %13s %17s \n",  "Name", "Quantity", "Unit Price", "Total Price" ) ;
         printf( "-------------------------------------------------------------- \n" ) ;
-        for( i = 0 ; i < NumberOfItems ; i++ ) {
+        for( i = 0 ; i < NumberOfItems ; i ++ ) {
             printf( "%-20s %11d %13.2f %17.2f \n", Name[i], Quantity[i] , UnitPrice[i], TotalPrice[i] ) ;
-        }//end for
+        }
         printf( "-------------------------------------------------------------- \n" ) ;
         printf( "%52s %11.2f \n", "TOTAL AMOUNT : ", TotalAmount ) ;
         printf( "Enter customer name [QUIT to stop] : " ) ;
         scanf( "%s", CustomerName ) ;
-    }//end while
+    }
     return 0 ;
-}//end function
+}
