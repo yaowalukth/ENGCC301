84bd2f1815d421d65bf36f757295b78d906a553f
yaowalukth <16917yaowaluk@gmail.com>
2024-10-05 18:56:55 +0700

    Update LAB5-1.c

--- Lab5/LAB5-1.c
+++ Lab5/LAB5-1.c
@@ -3,24 +3,24 @@
 int main() {
     float score = 0 ;
     printf( "Input your score : " ) ;
-    if(scanf( "%f" , &score  ) !=1 ) {
-           printf( "Please enter number only") ;
+    if(scanf( "%f" , &score  ) != 1 ) {
+        printf( "Please enter number only") ;
     } else if( score >= 80 ) {
-            printf( "Grade : A" ) ;
+        printf( "Grade : A !" ) ;
     } else if( score >= 75 && score < 80 ) {
-            printf( "Grade : B+" ) ;
+        printf( "Grade : B+ !" ) ;
     } else if( score >= 70 && score < 75 ) {
-            printf( "Grade : B" ) ;
+        printf( "Grade : B !" ) ;
     } else if( score >= 65 && score < 70 ) {
-            printf( "Grade : C+" ) ;
+        printf( "Grade : C+ !" ) ;
     } else if( score >= 60 && score < 65 ) {
-            printf( "Grade : C" ) ;
+        printf( "Grade : C !" ) ;
     } else if( score >= 55 && score < 60 ) {
-            printf( "Grade : D+" ) ;
+        printf( "Grade : D+ !" ) ;
     } else if( score >= 50 && score < 55 ) {
-            printf( "Grade : D" ) ;
+        printf( "Grade : D !" ) ;
     } else {
-            printf( "Grade : F \n" ) ;
+        printf( "Grade : F !\n" ) ;
     }
     return 0 ;
 }
