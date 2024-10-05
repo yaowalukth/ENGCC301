e4db0ef1acfe306c5a2a32fccf18170873889f63
yaowalukth <16917yaowaluk@gmail.com>
2024-10-05 18:06:47 +0700

    Update LAB1.c

--- LAB1.c
+++ LAB1.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello world\n");
-    printf("My name JAMES\n");
-    return 0;
+    char Name[50] ;
+    int  Age = 0 ;
+    printf( "Enter your name: " ) ;
+    scanf( "%s", Name ) ;
+    printf( "Enter your age: " ) ;
+    scanf( "%d", &Age ) ;
+    printf( "- - - - - -\n" ) ;
+    printf( "Hello %s \n", Name ) ; 
+    printf( "Age = %d\n", Age ) ; 
+    return 0 ;
 }
