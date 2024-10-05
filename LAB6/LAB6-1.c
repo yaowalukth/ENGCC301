b406e8975bc6095298d2fa18b4b03440f33299da
yaowalukth <16917yaowaluk@gmail.com>
2024-10-05 22:07:53 +0700

    Update LAB6-1.c

--- LAB6/LAB6-1.c
+++ LAB6/LAB6-1.c
@@ -1,16 +1,17 @@
-#include <stdio.h>
+##include <stdio.h>
 
 int main() {
     int number ;
-    printf( "Enter your number : " ) ;
+    printf( "Enter value: " ) ;
     scanf( "%d" , &number ) ;
+    printf( "Series:" ) ;
     if(number % 2 == 1) {
         for(int i = 1 ; i <= number ; i += 2 ) {
-            printf( "%d ", i ) ;
+            printf( " %d ", i ) ;
         }
     }else if(number % 2 != 1) {
         for(int i = number ; i >= 0 ; i -= 2) {
-            printf( "%d " , i ) ;
+            printf( " %d " , i ) ;
         }
     }
     return 0 ;
