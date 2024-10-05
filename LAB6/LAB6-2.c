1d3f61fc3259734d648396c9b625010aa46bfb4b
yaowalukth <16917yaowaluk@gmail.com>
2024-10-05 18:00:48 +0700

    Update and rename LAB6-2.cpp to LAB6-2.c

--- LAB6/LAB6-2.c
+++ LAB6/LAB6-2.c
@@ -8,7 +8,7 @@ int main() {
         for (int b = 1 ; b <= num ; b++ ) {
             if( (num % 2 != 0 && b == num - a + 1 ) || ( num % 2 == 0 && b == a )) {
                 printf( "1" );
-            }else{
+            } else {
                 printf( "0" ) ;
             }
         }
