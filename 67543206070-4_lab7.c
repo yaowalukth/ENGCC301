d11f223150f02a1cd0b51b1436205ec523088fc2
yaowalukth <16917yaowaluk@gmail.com>
2024-10-06 22:45:02 +0700

    Update 67543206070-4_lab7.c

--- 67543206070-4_lab7.c
+++ 67543206070-4_lab7.c
@@ -22,7 +22,7 @@ int main() {
             srand( time( NULL ) ) ;
 
             while( score > 0  ) {
-                printf( "Guess  the winning number (%d-%d): ", min , max ) ;
+                printf( "Guess the winning number (%d-%d): ", min , max ) ;
                 scanf( "%d", &guess ) ;
                 if( guess < 1 || guess > 100 ) {
                     printf( "Your guess is out of the current bounds (%d-%d)! Try again. \n", min , max ) ;
@@ -44,16 +44,16 @@ int main() {
                         if( guess > min ) {
                             min = guess + 1 ;
                         }
-                        printf( "Sorry,the winning number is HIGHER than %d. (Score=%d) \n", guess , score ) ;
+                        printf( "Sorry, the winning number is HIGHER than %d. (Score=%d) \n", guess , score ) ;
                     } else {
                         if( guess < max ) {
                             max = guess -1 ;
                         }
-                        printf( "Sorry,the winning number is LOWER than %d. (Score=%d)\n", guess , score ) ;
+                        printf( "Sorry, the winning number is LOWER than %d. (Score=%d)\n", guess , score ) ;
                     }
                 }
             }
-            printf( "Do you want to play (1=play, -1=exit) : \n" ) ;
+            printf( "Do you want to play game (1=play, -1=exit) : \n" ) ;
             scanf( "%d", &playAgain ) ;
         }while( playAgain == 1 ) ;
     } 
